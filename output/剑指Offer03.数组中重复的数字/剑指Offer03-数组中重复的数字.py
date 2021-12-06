bcac9066bbaf2c7df0aa0b8fee3ef95b0dca48c1
quzard <1191890118@qq.com>
2021-12-06 17:27:18 +0800

    2021-12-06

--- output/剑指Offer03.数组中重复的数字/剑指Offer03-数组中重复的数字.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def findRepeatNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range(len(nums) - 1, 0 , -1):
-            if nums[i] == nums[i-1]:
-                return nums[i]
