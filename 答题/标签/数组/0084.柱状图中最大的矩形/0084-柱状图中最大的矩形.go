d7cc5f54b783b10179ac3a64bbe53e25552861d7
quzard <1191890118@qq.com>
2022-01-13 16:32:18 +0800

    2022-01-13

--- 答题/标签/数组/0084.柱状图中最大的矩形/0084-柱状图中最大的矩形.go
+++ 答题/标签/数组/0084.柱状图中最大的矩形/0084-柱状图中最大的矩形.go
@@ -12,7 +12,7 @@ func largestRectangleArea1(heights []int) int {
     return res
 }
 
-func largestRectangleArea(heights []int) int {
+func largestRectangleArea2(heights []int) int {
     heights = append([]int{0}, heights...)
     heights = append(heights, 0)
     var minStack = make([]int, len(heights))
@@ -64,4 +64,39 @@ func min(a, b int) int{
         return a
     }
     return b
+}
+
+// 单调栈
+func largestRectangleArea(heights []int) int {
+	n := len(heights)
+	left, right := make([]int, n), make([]int, n)
+	mono_stack := []int{}
+	for i := 0; i < n; i++ {
+		for len(mono_stack) > 0 && heights[mono_stack[len(mono_stack)-1]] >= heights[i] {
+			mono_stack = mono_stack[:len(mono_stack)-1]
+		}
+		if len(mono_stack) == 0 {
+			left[i] = -1
+		} else {
+			left[i] = mono_stack[len(mono_stack)-1]
+		}
+		mono_stack = append(mono_stack, i)
+	}
+	mono_stack = []int{}
+	for i := n - 1; i >= 0; i-- {
+		for len(mono_stack) > 0 && heights[mono_stack[len(mono_stack)-1]] >= heights[i] {
+			mono_stack = mono_stack[:len(mono_stack)-1]
+		}
+		if len(mono_stack) == 0 {
+			right[i] = n
+		} else {
+			right[i] = mono_stack[len(mono_stack)-1]
+		}
+		mono_stack = append(mono_stack, i)
+	}
+	ans := 0
+	for i := 0; i < n; i++ {
+		ans = max(ans, (right[i]-left[i]-1)*heights[i])
+	}
+	return ans
 }
